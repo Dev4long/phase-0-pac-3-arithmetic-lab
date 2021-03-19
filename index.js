10a11b1df6e5a5d75c85e62c65f8018f654e18b5
deniskadrich <dev4long@live.com>
2021-03-19 12:50:54 -0400

    completed assignment

---
+++ index.js
@@ -0,0 +1,38 @@
+function add (a, b) {
+    return add (a+b)
+}
+function subtract (a, b) {
+    return subtract (a-b)
+}
+function multiply (a, b) {
+    return multiply (a*b)
+}
+function divide (a, b) {
+    return divide (a/b)
+}
+a =10
+b= 2
+function add (a, b) {
+    return (a+b)
+}
+function subtract (a, b) {
+    return (a-b)
+}
+function multiply (a, b) {
+    return (a*b)
+}
+function divide (a, b) {
+    return (a/b)
+}
+function increment (a){
+    return (a + 1)
+}
+function decrement (a){
+    return (a-1)
+}
+function makeInt (a) {
+    return (parseInt(a, 10))
+}
+function preserveDecimal (n){
+    return (parseFloat(n))
+}
